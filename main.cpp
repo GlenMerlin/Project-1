fc0785c1dcc526d7928c9a81fa66169eac54ab2f
GlenMerlin <Glenmerlin@protonmail.com>
2022-09-16 11:20:57 -0600

    comments detailing next steps

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "Lexer.h"
+#include <iostream>
 
 int main(int argc, char** argv) {
+
+    std::cout << "argc: " << argc << std::endl << "argv: " << argv[1] << std::endl;  
+    // TODO: code to read the file in
 
     Lexer* lexer = new Lexer();
 
